54c6596cb213315bc09dee8669ea3ea69db36571
V <116948898+raazrifany@users.noreply.github.com>
2023-11-09 21:44:47 +0700

    Add files via upload

---
+++ praktikum4_1.java
@@ -0,0 +1,11 @@
+public class praktikum4_1 {
+
+    public static void main(String args[]) {
+    	int i;
+    	for(i=1; i<=5; i++){
+    		System.out.println("Looping ke-"+i);
+    	}
+    }
+    
+    
+}
